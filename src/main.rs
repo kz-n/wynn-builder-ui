5cc34351300420557e1720cc604a62166833d743
TYTheBeast <thomasyou03@gmail.com>
2024-11-02 09:28:12 +0100

    Progress on the Config tab

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use iced::alignment::{Horizontal, Vertical};
 use iced::widget::{button, checkbox, column, container, pick_list, row, text, text_input};
 use iced::{Element, Length, Renderer, Task, Theme};
 use iced_widget::text_editor::Action;
-use iced_widget::{text_editor, Container};
+use iced_widget::{combo_box, text_editor, Container};
 use serde::{Deserialize, Serialize};
+
+mod theme_serde;
 
 #[derive(Debug, Clone, PartialEq, Default)]
 enum Tab {
@@ -26,18 +28,132 @@ enum Message {
     SearchItemEditorAction(Action),
     SearchItemStdout(String),
     Ignore, // For readonly text input
-    ConfigFileEditorAction(Action),
+    HelmetSelected(String),
+    ChestplateSelected(String),
+    LeggingsSelected(String),
+    BootsSelected(String),
+    RingsSelected(String),
+    BraceletsSelected(String),
+    NecklacesSelected(String),
+    WeaponsSelected(String),
 }
 
 #[derive(Default)]
 struct Tabs {
     active_tab: Tab,
     theme: Theme,
+    search_items_tab: SearchItemsTab,
+    config_file_tab: ConfigFileTab,
+}
+
+#[derive(Default)]
+struct SearchItemsTab {
     search_input: String,
     search_results: text_editor::Content,
-    config_file_content: text_editor::Content,
 }
-mod theme_serde;
+
+#[derive(Default)]
+struct ConfigFileTab {
+    helmets: combo_box::State<String>,
+    selected_helmet: Option<String>,
+    chestplates: combo_box::State<String>,
+    selected_chestplate: Option<String>,
+    leggings: combo_box::State<String>,
+    selected_leggings: Option<String>,
+    boots: combo_box::State<String>,
+    selected_boots: Option<String>,
+    rings: combo_box::State<String>,
+    selected_rings: Option<String>,
+    bracelets: combo_box::State<String>,
+    selected_bracelets: Option<String>,
+    necklaces: combo_box::State<String>,
+    selected_necklaces: Option<String>,
+    weapons: combo_box::State<String>,
+    selected_weapon: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct GearList {
+    items: Vec<Gear>,
+}
+
+impl GearList {
+    fn from_json(path: &str) -> Self {
+        let items_json_string = std::fs::read_to_string(path).unwrap();
+        serde_json::from_str(&items_json_string).unwrap()
+    }
+
+    fn get_gear_by_type(&self, gear_type: GearType) -> Vec<String> {
+        self.items
+            .iter()
+            .filter(|gear| gear.gear_type == gear_type)
+            .map(|gear| gear.name.clone())
+            .collect()
+    }
+
+    fn helmets(&self) -> Vec<String> {
+        self.get_gear_by_type(GearType::Helmet)
+    }
+
+    fn chestplates(&self) -> Vec<String> {
+        self.get_gear_by_type(GearType::Chestplate)
+    }
+
+    fn leggings(&self) -> Vec<String> {
+        self.get_gear_by_type(GearType::Leggings)
+    }
+
+    fn boots(&self) -> Vec<String> {
+        self.get_gear_by_type(GearType::Boots)
+    }
+
+    fn rings(&self) -> Vec<String> {
+        self.get_gear_by_type(GearType::Ring)
+    }
+
+    fn bracelets(&self) -> Vec<String> {
+        self.get_gear_by_type(GearType::Bracelet)
+    }
+
+    fn necklaces(&self) -> Vec<String> {
+        self.get_gear_by_type(GearType::Necklace)
+    }
+
+    fn weapons(&self) -> Vec<String> {
+        self.get_gear_by_type(GearType::Spear)
+            .into_iter()
+            .chain(self.get_gear_by_type(GearType::Wand))
+            .chain(self.get_gear_by_type(GearType::Bow))
+            .chain(self.get_gear_by_type(GearType::Dagger))
+            .chain(self.get_gear_by_type(GearType::Relik))
+            .collect()
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Gear {
+    id: i64,
+    name: String,
+    #[serde(rename = "type")]
+    gear_type: GearType,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+enum GearType {
+    Helmet,
+    Chestplate,
+    Leggings,
+    Boots,
+    Ring,
+    Bracelet,
+    Necklace,
+    Spear,
+    Wand,
+    Bow,
+    Dagger,
+    Relik,
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 struct ThemeConfig {
@@ -60,18 +176,24 @@ impl Tabs {
             Err(_) => Theme::Dark,
         };
 
-        let config_file_content = match std::fs::read_to_string("config/config.toml") {
-            Ok(content) => content,
-            Err(_) => String::from("# Config file not found.\n# A new one will be created when you save."),
-        };
+        let gear_list = GearList::from_json("config/items.json");
 
         (
             Self {
                 active_tab: Tab::Intro,
                 theme,
-                search_input: String::new(),
-                search_results: text_editor::Content::new(),
-                config_file_content: text_editor::Content::with_text(&config_file_content),
+                config_file_tab: ConfigFileTab {
+                    helmets: combo_box::State::new(gear_list.helmets()),
+                    chestplates: combo_box::State::new(gear_list.chestplates()),
+                    leggings: combo_box::State::new(gear_list.leggings()),
+                    boots: combo_box::State::new(gear_list.boots()),
+                    rings: combo_box::State::new(gear_list.rings()),
+                    bracelets: combo_box::State::new(gear_list.bracelets()),
+                    necklaces: combo_box::State::new(gear_list.necklaces()),
+                    weapons: combo_box::State::new(gear_list.weapons()),
+                    ..Default::default()
+                },
+                search_items_tab: SearchItemsTab::default(),
             },
             Task::none(),
         )
@@ -95,7 +217,7 @@ impl Tabs {
                 let _ = std::fs::write(theme_path, theme_toml);
             }
             Message::SearchInputChanged(input_text) => {
-                self.search_input = input_text;
+                self.search_items_tab.search_input = input_text;
             }
             Message::SearchInputSubmitted => {
                 let binary_name = if cfg!(windows) {
@@ -105,94 +227,90 @@ impl Tabs {
                 };
 
                 // Split input into arguments, preserving quoted strings if present
-                let args: Vec<&str> = self.search_input.split_whitespace().collect();
+                let args: Vec<&str> = self
+                    .search_items_tab
+                    .search_input
+                    .split_whitespace()
+                    .collect();
 
                 let output = match std::process::Command::new(binary_name).args(args).output() {
-                    Ok(output) => {
-                        match output.status.success() {
-                            true => String::from_utf8_lossy(&output.stdout).to_string(),
-                            false => String::from_utf8_lossy(&output.stderr).to_string(),
-                        }
-                    }
+                    Ok(output) => match output.status.success() {
+                        true => String::from_utf8_lossy(&output.stdout).to_string(),
+                        false => String::from_utf8_lossy(&output.stderr).to_string(),
+                    },
                     Err(e) => format!("Error: Could not execute search_item binary: {}", e),
                 };
 
-                self.search_results = text_editor::Content::with_text(&output);
+                self.search_items_tab.search_results = text_editor::Content::with_text(&output);
             }
             Message::Ignore => (),
             Message::SearchItemStdout(output) => {
-                self.search_results = text_editor::Content::with_text(&output);
+                self.search_items_tab.search_results = text_editor::Content::with_text(&output);
             }
             Message::SearchItemEditorAction(action) => {
                 match action {
                     Action::Edit(_) => (), // Do nothing for edits
                     Action::Move(_) => (), // Do nothing for moves
                     Action::Select(motion) => {
-                        self.search_results
+                        self.search_items_tab
+                            .search_results
                             .perform(Action::Select(motion));
                     }
                     Action::SelectWord => {
-                        self.search_results.perform(Action::SelectWord);
+                        self.search_items_tab
+                            .search_results
+                            .perform(Action::SelectWord);
                     }
                     Action::SelectLine => {
-                        self.search_results.perform(Action::SelectLine);
+                        self.search_items_tab
+                            .search_results
+                            .perform(Action::SelectLine);
                     }
                     Action::SelectAll => {
-                        self.search_results.perform(Action::SelectAll);
+                        self.search_items_tab
+                            .search_results
+                            .perform(Action::SelectAll);
                     }
                     Action::Click(point) => {
-                        self.search_results
+                        self.search_items_tab
+                            .search_results
                             .perform(Action::Click(point));
                     }
                     Action::Drag(point) => {
-                        self.search_results
+                        self.search_items_tab
+                            .search_results
                             .perform(Action::Drag(point));
                     }
                     Action::Scroll { lines } => {
-                        self.search_results
+                        self.search_items_tab
+                            .search_results
                             .perform(Action::Scroll { lines });
                     }
                 }
             }
-            Message::ConfigFileEditorAction(action) => {
-                match action {
-                    Action::Edit(edit) => {
-                        self.config_file_content.perform(Action::Edit(edit));
-                        // Write the updated content to file
-                        if let Ok(_) = std::fs::write("config/config.toml", self.config_file_content.text()) {
-                            // Successfully saved
-                        }
-                    },
-                    Action::Move(movement) => {
-                        // Enable cursor movement
-                        self.config_file_content.perform(Action::Move(movement));
-                    },
-                    Action::Select(motion) => {
-                        self.search_results
-                            .perform(Action::Select(motion));
-                    }
-                    Action::SelectWord => {
-                        self.search_results.perform(Action::SelectWord);
-                    }
-                    Action::SelectLine => {
-                        self.search_results.perform(Action::SelectLine);
-                    }
-                    Action::SelectAll => {
-                        self.search_results.perform(Action::SelectAll);
-                    }
-                    Action::Click(point) => {
-                        self.search_results
-                            .perform(Action::Click(point));
-                    }
-                    Action::Drag(point) => {
-                        self.search_results
-                            .perform(Action::Drag(point));
-                    }
-                    Action::Scroll { lines } => {
-                        self.search_results
-                            .perform(Action::Scroll { lines });
-                    }
-                }
+            Message::HelmetSelected(name) => {
+                self.config_file_tab.selected_helmet = Some(name);
+            }
+            Message::ChestplateSelected(name) => {
+                self.config_file_tab.selected_chestplate = Some(name);
+            }
+            Message::LeggingsSelected(name) => {
+                self.config_file_tab.selected_leggings = Some(name);
+            }
+            Message::BootsSelected(name) => {
+                self.config_file_tab.selected_boots = Some(name);
+            }
+            Message::RingsSelected(name) => {
+                self.config_file_tab.selected_rings = Some(name);
+            }
+            Message::BraceletsSelected(name) => {
+                self.config_file_tab.selected_bracelets = Some(name);
+            }
+            Message::NecklacesSelected(name) => {
+                self.config_file_tab.selected_necklaces = Some(name);
+            }
+            Message::WeaponsSelected(name) => {
+                self.config_file_tab.selected_weapon = Some(name);
             }
         }
     }
@@ -261,14 +379,14 @@ impl Tabs {
                     // Search input
                     text_input(
                         "Enter search parameters...",
-                        &self.search_input
+                        &self.search_items_tab.search_input
                     )
                     .on_input(|input| Message::SearchInputChanged(input))
                     .on_submit(Message::SearchInputSubmitted)
                     .padding(10)
                     .width(Length::Fill),
                     text_editor(
-                        &self.search_results
+                        &self.search_items_tab.search_results
                     )
                     .placeholder("Output will appear here...")
                     .on_action(Message::SearchItemEditorAction)/* 
@@ -327,14 +445,56 @@ impl Tabs {
             }
             Tab::ConfigFile => {
                 let column = column![
-                    text("Edit Configuration File")
-                        .size(30),
-                    text("The configuration will be saved automatically when you edit.")
-                        .size(20),
-                    text_editor(
-                        &self.config_file_content
-                    )
-                    .on_action(Message::ConfigFileEditorAction)
+                    text("Edit Configuration File").size(30),
+                    text("The configuration will be saved automatically when you edit.").size(20),
+                    combo_box(
+                        &self.config_file_tab.helmets,
+                        "Select a helmet...",
+                        self.config_file_tab.selected_helmet.as_ref(),
+                        Message::HelmetSelected
+                    ),
+                    combo_box(
+                        &self.config_file_tab.chestplates,
+                        "Select a chestplate...",
+                        self.config_file_tab.selected_chestplate.as_ref(),
+                        Message::ChestplateSelected
+                    ),
+                    combo_box(
+                        &self.config_file_tab.leggings,
+                        "Select a leggings...",
+                        self.config_file_tab.selected_leggings.as_ref(),
+                        Message::LeggingsSelected
+                    ),
+                    combo_box(
+                        &self.config_file_tab.boots,
+                        "Select a boots...",
+                        self.config_file_tab.selected_boots.as_ref(),
+                        Message::BootsSelected
+                    ),
+                    combo_box(
+                        &self.config_file_tab.rings,
+                        "Select a ring...",
+                        self.config_file_tab.selected_rings.as_ref(),
+                        Message::RingsSelected
+                    ),
+                    combo_box(
+                        &self.config_file_tab.bracelets,
+                        "Select a bracelet...",
+                        self.config_file_tab.selected_bracelets.as_ref(),
+                        Message::BraceletsSelected
+                    ),
+                    combo_box(
+                        &self.config_file_tab.necklaces,
+                        "Select a necklace...",
+                        self.config_file_tab.selected_necklaces.as_ref(),
+                        Message::NecklacesSelected
+                    ),
+                    combo_box(
+                        &self.config_file_tab.weapons,
+                        "Select a weapon...",
+                        self.config_file_tab.selected_weapon.as_ref(),
+                        Message::WeaponsSelected
+                    ),
                 ]
                 .spacing(20)
                 .align_x(Horizontal::Center);
@@ -345,7 +505,7 @@ impl Tabs {
                     .width(Length::Fill)
                     .height(Length::Fill)
                     .into()
-            },
+            }
             Tab::Builder => container(column![text("Content for Builder Tab")]).into(),
         };
 
